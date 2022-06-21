d642374b21f43e615c3b81cfbb3574c20bcf8401
jetfire679 <jetfire679@gmail.com>
2022-06-21 11:54:46 -0400

    updating network.tf

--- network.tf
+++ network.tf
@@ -4,12 +4,12 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = join("-", [var.vApp,"vpc"])
-  cidr = "10.0.0.0/16"
+  cidr = "10.1.0.0/16"
 
 
 
   azs            = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  public_subnets = ["10.1.0.0/24","10.2.1.0/24","10.3.2.0/24"]
+  public_subnets = ["10.1.0.0/24","10.1.1.0/24","10.1.2.0/24"]
   # private_subnets  = ["10.0.10.0/24","10.0.20.0/24","10.0.30.0/24"]
 
   # enable_nat_gateway = true
